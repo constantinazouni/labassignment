cfbc7e577c38216e6bcea63b7b7af823275bce0f
constantinazouni <t8190046@aueb.gr>
2022-05-18 09:40:51 +0300

    Add invalid input testing that fails

--- testing/src/test/java/io_test/FileIOTest.java
+++ testing/src/test/java/io_test/FileIOTest.java
@@ -15,7 +15,7 @@ public class FileIOTest {
   public ExpectedException exp = ExpectedException.none();
 
   @Test
-  public void readFile_validArrayInputTesting() {
+  public void testReadFile_validArrayInput() {
     int[] expected_output = {2, 3, 4, 6, -1, 10, 7, -2, 1, 1, 2, 18, 10, 20,
         24, -1, -4, 22, 22, 444, 700, 1000};
     filepath = new File("src/test/resources/valid_input.txt").getAbsolutePath();
@@ -23,7 +23,7 @@ public class FileIOTest {
   }
   
   @Test
-  public void readFile_notExistingInputFile() {
+  public void testReadFile_notExistingInputFile() {
     filepath = new File("src/test/resources/not_existing_title.txt").getAbsolutePath();
     exp.expect(IllegalArgumentException.class);
     exp.expectMessage("Input file does not exist");
@@ -31,11 +31,19 @@ public class FileIOTest {
   }
 
   @Test
-  public void readFile_emptyFile() {
+  public void testReadFile_emptyFile() {
     filepath = new File("src/test/resources/empty.txt").getAbsolutePath();
     exp.expect(IllegalArgumentException.class);
     exp.expectMessage("Given file is empty");
     fileio.readFile(filepath);
+  }
+
+  @Test
+  public void testReadFileContainsInvalidEntries() {
+    int[] expectedInvalidSample = {2, 3, 4, 6, -1, 10, 7, -2, 1, 1, 2, "aa", 10, 20,
+      24, -1, -4, 22, 22, 10.2, 700, 1000};
+    filepath = new File("src/test/resources/invalid_input.txt").getAbsolutePath();
+    Assert.assertArrayEquals(expectedInvalidSample, fileio.readFile(filepath));
   }
 
 }

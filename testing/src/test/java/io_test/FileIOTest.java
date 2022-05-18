219942bd7211745fc01a596d492458be8a3b41db
constantinazouni <t8190046@aueb.gr>
2022-05-18 09:33:16 +0300

    Add illegal argument test cases to FileIO testing

--- testing/src/test/java/io_test/FileIOTest.java
+++ testing/src/test/java/io_test/FileIOTest.java
@@ -15,13 +15,27 @@ public class FileIOTest {
   public ExpectedException exp = ExpectedException.none();
 
   @Test
-  public void testReadFile_validArrayInputTesting() {
-    int[] expectedValidSample = {2, 3, 4, 6, -1, 10, 7, -2, 1, 1, 2, 18, 10, 20,
+  public void readFile_validArrayInputTesting() {
+    int[] expected_output = {2, 3, 4, 6, -1, 10, 7, -2, 1, 1, 2, 18, 10, 20,
         24, -1, -4, 22, 22, 444, 700, 1000};
     filepath = new File("src/test/resources/valid_input.txt").getAbsolutePath();
-    Assert.assertArrayEquals(expectedValidSample, fileio.readFile(filepath));
+    Assert.assertArrayEquals(expected_output, fileio.readFile(filepath));
+  }
+  
+  @Test
+  public void readFile_notExistingInputFile() {
+    filepath = new File("src/test/resources/not_existing_title.txt").getAbsolutePath();
+    exp.expect(IllegalArgumentException.class);
+    exp.expectMessage("Input file does not exist");
+    fileio.readFile(filepath);
   }
 
-  
+  @Test
+  public void readFile_emptyFile() {
+    filepath = new File("src/test/resources/empty.txt").getAbsolutePath();
+    exp.expect(IllegalArgumentException.class);
+    exp.expectMessage("Given file is empty");
+    fileio.readFile(filepath);
+  }
 
 }
